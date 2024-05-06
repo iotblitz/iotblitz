5d047e85fb3dd74d0ba71a8d3251229f726b3356
Amit Mondal <amit.ndk3@gmail.com>
2024-05-06 15:27:15 +0530

    add top bar

---
+++ resources/views/public/common/topbar.blade copy.php
@@ -0,0 +1,84 @@
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        {{-- <h1>Scholar</h1> --}}
+                        <img src="{{ asset('public/public_page') }}/assets/images/logo.png" class="logo" alt="" width="30%" style="position: absolute;">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Serach Start ***** -->
+=======
+<!--header area start-->
+<div class="header-area wow fadeInDown header-absolate" id="nav" data-0="position:fixed;" data-top-top="position:fixed;top:0;" data-edge-strategy="set">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 d-block d-lg-none">
+                    <div class="mobile-menu"></div>
+                </div>
+                <div class="col-4 col-lg-2">
+                    <div class="logo-area">
+                        <a href="#"><img src="{{ asset('public/public_page') }}/lottiefiles/logo.png" alt="" width="50%" height="100%"></a>
+                    </div>
+                </div>
+                <div class="col-4 col-lg-8 d-none d-lg-block">
+                    <div class="main-menu text-center">
+                        <nav>
+                            <ul id="slick-nav">
+                                <li><a class="scroll" href="{{route('home')}}">home</a>
+                                    {{-- <ul>
+                                        <li><a href="index.html">Version 1</a></li>
+                                        <li><a href="index2.html">Version 2</a></li>
+                                        <li><a href="blog.html">blog</a></li>
+                                        <li><a href="single-blog.html">single blog</a></li>
+                                    </ul> --}}
+                                </li>
+                                <li><a class="scroll" href="#about">About</a>
+                                </li>
+                                <li><a class="scroll" href="{{route('case_study')}}">Case Study</a></li>
+                                <li><a class="scroll" href="#token">Products</a></li>
+                                <li><a class="scroll" href="#roadmap">solutions</a></li>
+                                <li><a class="scroll" href="{{route('blogs')}}">Blogs</a></li>
+                                <li><a class="scroll" href="#faq">FAQ</a></li>
+                                <li><a class="scroll" href="#contact">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-4 col-lg-2 text-right">
+                    <a href="#" class="logibtn gradient-btn">Call Now</a>
+                </div>
+>>>>>>> ccb3badef9e34246df36ccbf2a2274f166685830
+
+                    <!-- ***** Serach Start ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                      <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                      <li class="scroll-to-section"><a href="#services">Services</a></li>
+                      <li class="scroll-to-section"><a href="#courses">Courses</a></li>
+                      <li class="scroll-to-section"><a href="#team">Team</a></li>
+                      <li class="scroll-to-section"><a href="#events">Events</a></li>
+                      <li class="scroll-to-section"><a href="#contact">Register Now!</a></li>
+                  </ul>
+
+                  <div class="search-input" style="margin-left: 10%">
+                    <div id="search" action="#">
+                      <input type="button" value="Contact Now" id='searchText' />
+                      {{-- <i class="fa fa-search"></i> --}}
+                      <i class="fa-regular fa-paper-plane"></i>
+                    </div>
+                  </div>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
