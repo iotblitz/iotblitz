8369de573436cf25aa94a10d31e997f640133fb9
Amit Mondal <amit.ndk3@gmail.com>
2024-05-06 15:37:06 +0530

    add topbar

--- resources/views/public/common/topbar.blade.php
+++ resources/views/public/common/topbar.blade.php
@@ -1,6 +1,5 @@
-
-  <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky">
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky">
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -8,7 +7,8 @@
                     <!-- ***** Logo Start ***** -->
                     <a href="index.html" class="logo">
                         {{-- <h1>Scholar</h1> --}}
-                        <img src="{{ asset('public/public_page') }}/assets/images/logo.png" class="logo" alt="" width="30%" style="position: absolute;">
+                        <img src="{{ asset('public/public_page') }}/assets/images/logo.png" class="logo" alt=""
+                            width="30%" style="position: absolute;">
                     </a>
                     <!-- ***** Logo End ***** -->
                     <!-- ***** Serach Start ***** -->
@@ -16,24 +16,31 @@
                     <!-- ***** Serach Start ***** -->
                     <!-- ***** Menu Start ***** -->
                     <ul class="nav">
-                      <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                      <li class="scroll-to-section"><a href="#services">About</a></li>
-                      <li class="scroll-to-section"><a href="#courses">Case Study</a></li>
-                      <li class="scroll-to-section"><a href="#team">Products</a></li>
-                      <li class="scroll-to-section"><a href="#events">Solutions</a></li>
-                      <li class="scroll-to-section"><a href="#events">Blogs</a></li>
-                      <li class="scroll-to-section"><a href="#events">Blogs</a></li>
-                      <li class="scroll-to-section"><a href="#events">FAQ</a></li>
-                      <li class="scroll-to-section"><a href="#contact">Contact</a></li>
-                  </ul>
+                        <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                        <li class="scroll-to-section"><a href="#services">About</a></li>
+                        <li class="scroll-to-section"><a href="#courses">Case Study</a></li>
+                        <li class="scroll-to-section"><a href="#team">Products</a></li>
 
-                  <div class="search-input" style="margin-left: 10%">
-                    <div id="search" action="#">
-                      <input type="button" value="Contact Now" id='searchText' />
-                      {{-- <i class="fa fa-search"></i> --}}
-                      <i class="fa-regular fa-paper-plane"></i>
+
+                        <li class="scroll-to-section">
+                            <a href="#events">Solutions</a>
+                            <ul class="submenu">
+                                <li><a href="#events">Solutions</a></li>
+                                <li><a href="#events">Blogs</a></li>
+                                <li><a href="#events">FAQ</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="scroll-to-section"><a href="#contact">Contact</a></li>
+                    </ul>
+
+                    <div class="search-input" style="margin-left: 10%">
+                        <div id="search" action="#">
+                            <input type="button" value="Contact Now" id='searchText' />
+                            {{-- <i class="fa fa-search"></i> --}}
+                            <i class="fa-regular fa-paper-plane"></i>
+                        </div>
                     </div>
-                  </div>
                     <a class='menu-trigger'>
                         <span>Menu</span>
                     </a>
@@ -42,5 +49,5 @@
             </div>
         </div>
     </div>
-  </header>
-  <!-- ***** Header Area End ***** -->
+</header>
+<!-- ***** Header Area End ***** -->
