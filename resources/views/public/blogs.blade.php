1f615dfe2d128416c7384fe361598a912f4a0750
Amit Mondal <amit.ndk3@gmail.com>
2024-05-22 17:35:34 +0530

    fixed blog

--- resources/views/public/blogs.blade.php
+++ resources/views/public/blogs.blade.php
@@ -1,82 +1,100 @@
 @extends('public.common.layout')
 @section('page_content')
-    <!--welcome area start-->
-    <div class="welcome-area wow fadeInUp" id="home">
-        <div id="particles-js"></div>
-        <div class="container">
-            <div class="row">
 
-                <div class="col-12 col-md-6 align-self-center">
-                    <div class="welcome-right">
-                        <div class="welcome-text">
-                            <h1>Blogs</h1>
-                            <h4>Sifting through teaspoons of clay and sand scraped from the
-                                floors of caves, German researchers have managed.</h4>
-                        </div>
+<style>
+    .main-banner {
+    padding: 50px 0;
+    }
 
-                    </div>
-                </div>
+    .header-text {
+    text-align: left;
+    }
 
-                <div class="col-12 col-md-6">
-                    <div class="welcome-img">
-                        <lottie-player src="{{ asset('public/public_page') }}/lottiefiles/content.json" background="transparent"
-                            speed="1" style="width: 80%; height: auto;" loop autoplay
-                            class="img-fluid"></lottie-player>
-                        <!-- <img src="{{ asset('public_page') }}/assets/img/welcome-img.png" alt=""> -->
-                    </div>
-                </div>
+    .buttons {
+    margin-top: 20px;
+    }
 
-            </div>
+    .lottie-animation {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    }
+
+    </style>
+
+<div class="main-banner" id="top">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-7">
+          <div class="header-text">
+
+            {{-- <span class="category text-white">Our Courses</span> --}}
+            <h2 class="text-white">Blogs</h2>
+            <p class="text-white">Scholar is a free CSS template designed by TemplateMo for online educational related websites. This layout is based on the famous Bootstrap v5.3.0 framework.</p>
+
+          </div>
         </div>
+        <div class="col-lg-5">
+          <div class="lottie-animation">
+            <lottie-player src="{{ asset('public/public_page') }}/assets/lottiefiles/Animation - 1715838982563.json" background="transparent" speed="1" style="width: 100%; height: 100%;" loop autoplay></lottie-player>
+          </div>
+        </div>
+      </div>
     </div>
-    <!--welcome area end-->
+  </div>
 
 
-    <!--blog area start-->
-    <div class="blog-v wow fadeInUp section-padding" id="blog">
-        <div class="container">
-            <div class="row">
-
-                @foreach ($blogs as $blogdata)
 
 
-                {{-- <div class="col-12 col-lg-4">
-                    <div class="single-blog wow fadeInUp">
-                        <div class="single-blog-image" style="background:url('{{ asset('blog_images') }}/{{$blogdata->blog_image}}')"></div>
-                        <div class="single-blog-text">
-                            <a href="single-blog.html">{{$blogdata->blog_title}}</a>
-                            <div class="space-10"></div>
-                            <small> <i class="fa fa-clock-o"></i> January 12th, 2018</small>
-                        </div>
-                    </div>
-                </div> --}}
 
-                <div class="col-12 col-lg-4">
-                    <div class="single-blog wow fadeInUp">
-                        <div class="single-blog-image" style="background:url('{{ asset('public/blog_images') }}/{{$blogdata->blog_image}}')"></div>
-                        <div class="single-blog-text">
-                            <a href="{{ route('single_blogs', [str_replace(' ', '-', $blogdata->blog_title).'-'.$blogdata->blog_id])}}">{{substr($blogdata->text_description, 0, 100)}}....</a>
-                            <div class="space-10"></div>
-                            <small> <i class="fa fa-clock-o"></i> January 12th, 2018</small>
-                            <!-- Add the blog title here -->
-                            <h4><a href="{{ route('single_blogs', [str_replace(' ', '-', $blogdata->blog_title).'-'.$blogdata->blog_id])}}">{{$blogdata->blog_title}}</a></h4>
+  <div class="section events" id="events">
+    <div class="container">
+      <div class="row">
+        {{-- <div class="col-lg-12 text-center">
+          <div class="section-heading">
+            <h6>Case Study</h6>
+            <h2>Upcoming Case Study</h2>
+          </div>
+        </div> --}}
+            @foreach ($blogs as $blogdata)
+                <div class="col-lg-12 col-md-6">
+                    <div class="item" onclick="location.href = '{{ route('single_blogs', [str_replace(' ', '-', $blogdata->blog_title).'-'.$blogdata->blog_id])}}';">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="image">
+                                    <img src="{{ asset('public/blog_images') }}/{{$blogdata->blog_image}}" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-5">
+                                <h4>{{ $blogdata->blog_title}}</h4>
+                                <p>{{substr($blogdata->text_description, 0, 100)}}....</p>
+                            </div>
+                            <div class="col-lg-3">
+                            <span>Date:</span>
+                            <h6>January 12th, 2018</h6>
+                            </div>
+                            <a href="{{ route('single_blogs', [str_replace(' ', '-', $blogdata->blog_title).'-'.$blogdata->blog_id])}}"><i class="fa fa-angle-right"></i></a>
+
                         </div>
                     </div>
                 </div>
+            @endforeach
+      </div>
+      <div class="col-12 col-lg-12">
+        <div class="space-50"></div>
+        {!! $blogs->links('vendor.pagination.my_public_bootstrap-4') !!}
+    </div>
+    </div>
+  </div>
 
-                @endforeach
 
 
 
-                <div class="col-12 col-lg-12">
 
-                    <div class="space-50"></div>
-                    {!! $blogs->links('vendor.pagination.bootstrap-4') !!}
-                    {{-- <div class="blog-blog-btn text-center">
 
-                        <a href="#" class="gradient-btn">learn more</a>
-                    </div> --}}
-                </div>
+
+
             </div>
         </div>
         <!--blog area end-->
@@ -89,3 +107,5 @@
 @endsection
 @section('page_script')
 @endsection
+
+
