bd61368543efa50a703bfe4291878ee3048c0dba
Amit Mondal <amit.ndk3@gmail.com>
2024-06-27 11:53:30 +0530

    add robots.txt

--- resources/views/robots.blade.php
+++ resources/views/robots.blade.php
@@ -1,1 +1,12 @@
-hello
+User-agent: *
+Disallow: /admin/
+Disallow: /private/
+Allow: /public/
+
+# Block specific crawler
+User-agent: BadBot
+Disallow: /
+
+# Allow another specific crawler to access everything
+User-agent: GoodBot
+Disallow:
